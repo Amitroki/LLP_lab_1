c3146bcd48d251c545e2259f7cd37627a9b11f8c
Алексей Русяев <RusyaevAA@outlook.com>
2024-10-10 00:44:09 +0400

    small changes

--- c_simplified_sorting.c
+++ c_simplified_sorting.c
@@ -7,18 +7,20 @@ void main() {
     int cnt;
     printf("\tPlease, enter the count of elements in your array: ");
     scanf("%d", &cnt);
-    int k = 0;
+    int i = 0;
     int digit;
     printf("\tPlease, enter your array separated by a space: ");
 input_array:
-    if (k < cnt) {
-        scanf("%d", &digit);
-        buffer[k] = digit;
-        k += 1;
-        goto input_array;
+    if (i >= cnt) {
+        goto end_input;
     }
-    int checker;
-    int i = 0;
+    scanf("%d", &digit);
+    buffer[i] = digit;
+    i += 1;
+    goto input_array;
+end_input:
+    int checker = 0;
+    i = 0;
 outer_loop_start:
     if (i >= cnt - 1) goto end_sort;
     checker = 0;
@@ -38,12 +40,12 @@ check_break:
     i += 1;
     goto outer_loop_start;
 end_sort:
+    i = 0;
     printf("\n\tThat's your array after my bubble sort: ");
-    int l = 0;
 print_array:
-    if (l < cnt) {
-        printf("%d ", buffer[l]);
-        l += 1;
+    if (i < cnt) {
+        printf("%d ", buffer[i]);
+        i += 1;
         goto print_array;
     }
 }
